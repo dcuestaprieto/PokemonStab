f5b1564c480dfeb6bd86e07847e5f35d4d56a1c2
Diego Cuesta <dcuestaprieto@gmail.com>
2025-03-15 12:01:21 +0100

    feat: inject ApiService on DataModule and first Response Model (TypeResponse)

---
+++ composeApp/src/commonMain/kotlin/org/diego/pokemonstab/data/remote/ApiService.kt
@@ -0,0 +1,12 @@
+package org.diego.pokemonstab.data.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import org.diego.pokemonstab.data.remote.response.TypeResponse
+
+class ApiService(private val client: HttpClient) {
+    suspend fun getSingleType(id:String):TypeResponse{
+        return client.get("/type/$id").body<TypeResponse>()
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/org/diego/pokemonstab/data/remote/response/TypeResponse.kt
@@ -0,0 +1,17 @@
+package org.diego.pokemonstab.data.remote.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TypeResponse(
+    /*
+    si tenemos en nuestro modelo de datos un nombre de parametro diferente al del backend
+    podemos ponerle el decorator @SerialName("nombre") donde nombre es el nombre del parametro
+    que llega de la response, por ejemplo si del backend nos llega un parametro id pero nosotros lo queremos
+    tener en la data class como paramId deberíamos hacer lo siguiente:
+    @SerialName("id") val paramName:String
+
+     */
+    val id:String,
+    val name:String
+)
