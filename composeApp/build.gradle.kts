3e9497c32523dea38bc1aef252c9b01d52e8c44b
Diego Cuesta <dcuestaprieto@gmail.com>
2025-03-15 13:48:52 +0100

    feat: add CMP viewmodel dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -55,6 +55,8 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.negotiation)
             implementation(libs.kotlin.serialization)
+
+            implementation(libs.viewmodel.compose)
         }
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
